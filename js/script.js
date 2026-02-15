4286d10bb5729cc3147aa9b1b2dd7345f1b1f54d
Kyian <you@example.com>
2026-02-15 20:55:51 +0100

    add adaptive layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// ======== Modal ========
 const backdrop = document.querySelector('.backdrop');
 const openModalBtn = document.querySelector('.modal-btn-open');
 const closeModalBtn = document.querySelector('.modal-btn-close');
@@ -8,3 +9,13 @@ const toggleModal = () => backdrop.classList.toggle('is-hidden');
 
 openModalBtn.addEventListener('click', toggleModal);
 closeModalBtn.addEventListener('click', toggleModal);
+
+// ======== Mobile menu ========
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
